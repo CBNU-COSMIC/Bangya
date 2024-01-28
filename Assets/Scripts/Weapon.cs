a5650d963df4010425ae5a33373d381fc23f1a8a
GAUDUL <limjsu12@naver.com>
2024-01-28 22:59:31 +0900

    [ADD]총 애니메이터 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] GameObject _hitEffect;
     [SerializeField] GameObject _player;
     [SerializeField] Camera _fpsCamera;
     [SerializeField] ParticleSystem _gunEffect;
-    [SerializeField] float _damage = 30f;
-    [SerializeField] float _range = 30f;
-    [SerializeField] int _maxBullet = 10;
-    [SerializeField] int _haveBullet = 100  
+    [SerializeField] float _damage = 30f; //총 위력
+    [SerializeField] float _range = 30f;  //총 범위
+    [SerializeField] int _maxBullet = 10; //최대 탄환 수
+    [SerializeField] int _haveBullet = 100; //가지고 있는 탄환 수
     int Bullet = 10; //초기값
     bool IsAiming = false;
+    bool IsReloading = false;
 
     void Start()
     {
@@ -22,42 +24,48 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")) //발사
         {
             if (Bullet <= 0) //총알 없음
             {
                 Debug.Log("No Bullet");
                 return;
             }
-            Shoot(); //발사
+            if (!IsReloading) Shoot(); //발사
+            else return;
         }
 
-        if (Input.GetButtonDown("Fire2")) Aim();
+        else if (Input.GetButtonDown("Fire2")) Aim();  //조준
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            if (_haveBullet <= 0) //가지고 있는 거 없음
+        else if (Input.GetKeyDown(KeyCode.R)) //장전
             {
-                Debug.Log("No Bullet");
-                return;
-            }
-            if (Bullet == _maxBullet) //이미 꽉 참
-            {
-                Debug.Log("It's full");
-                return;
-            }
-            Debug.Log("Loading");
-            Load();
+                if (_haveBullet <= 0) //남은 탄환 없음
+                {
+                    Debug.Log("No Bullet");
+                    return;
+                }
+                if (Bullet == _maxBullet) //이미 꽉 참
+                {
+                    Debug.Log("It's full");
+                    return;
+                }
+                Debug.Log("Reloading");
+                if (!IsReloading)
+                {
+                    IsReloading = true;
+                    Reload();
+                }
+                else return;
         }
     }
 
     void Shoot()
     {
-        _gunEffect.Play(); //이펙트 (나중에 좋은 거 찾을게요)
-        _player.transform.Translate(0, 0, -0.1f); //그냥 반동 실험 중
-        _player.transform.Rotate(0.1f, 0.1f, 0); //이것도 그냥 실험 중
-        Fire(); //진짜 발사
-        Bullet--;
+            _gunEffect.Play();
+            Fire(); //진짜 발사
+            GetComponent<Animator>().SetTrigger("recoil"); //반동 애니
+            Bullet--;
+            Invoke("RecoilToIdleTrigger", 0.5f); //원래 상태로
     }
     void Fire()
     {
@@ -66,7 +74,7 @@ public class Weapon : MonoBehaviour
         {
             EnemyHealth target = Hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
-            //HitEffect(); // 맞은 곳 반짝
+            HitEffect(Hit); // 맞은 곳 반짝
             target.TakeDamage(_damage);
         }
     }
@@ -81,22 +89,42 @@ public class Weapon : MonoBehaviour
         else
         {
             Debug.Log("Stop Aiming");
-            IsAiming = false;
+            IdleTrigger();
         }
         //뭘 해 야 할 까 ?
     }
 
-    void Load()
+    void Reload()
     {
-        //딜레이?
-        int AddBullet = _maxBullet - Bullet;
-        if(AddBullet > _haveBullet)
-        {
-            Bullet += _haveBullet;
-            _haveBullet = 0;
-            return;
-        }
-        _haveBullet -= AddBullet;
-        Bullet += AddBullet;
+            GetComponent<Animator>().SetTrigger("reload"); //장전 애니
+            int AddBullet = _maxBullet - Bullet;
+            if (AddBullet > _haveBullet)
+            {
+                Bullet += _haveBullet;
+                _haveBullet = 0;
+                return;
+            }
+            _haveBullet -= AddBullet;
+            Bullet += AddBullet;
+            Invoke("IdleTrigger", 1.8f); //원래 상태로
     }
+
+    void HitEffect(RaycastHit hit)
+    {
+        GameObject Effect = Instantiate(_hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(Effect,0.1f);
+    }
+
+    void IdleTrigger()
+    {
+        GetComponent<Animator>().SetTrigger("idle");
+        IsReloading = false;
+        IsAiming = false;
+    }
+
+    void RecoilToIdleTrigger()
+    {
+        GetComponent<Animator>().SetTrigger("idle");
+    }
+
 }
